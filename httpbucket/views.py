44b54af37baa95c7b095a0b673c7edb335e54cfa
Jeremy Boyd <boydjj@gmail.com>
2017-08-21 23:32:41 -0500

    Minor code cleanup

--- httpbucket/views.py
+++ httpbucket/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 
 
 def hello_world(request):
@@ -10,6 +10,14 @@ def hello_world(request):
 def echo_get(request):
     response = {}
 
+    if request.method != 'GET':
+        return HttpResponseNotAllowed(['GET'])
+
+    # Add what we know we can always get from the request
+    response['origin'] = request.META.get('REMOTE_ADDR')
+    response['url'] = request.get_raw_uri()
+
+    # Add any query parameters found in the URI
     args = {}
     for k, v in request.GET.lists():
         if len(v) == 1:
@@ -18,20 +26,22 @@ def echo_get(request):
             args[k] = v
     response['args'] = args
 
+    # Add any headers we find in the request
     headers = {}
     for k, v in request.META.items():
         if not v:
             continue
         new_key = None
+
+        # Content-Type and Content-Location aren't modified
         if k.startswith("CONTENT"):
             new_key = k
+        # Any other headers in the request are prepended with HTTP_
         elif k.startswith('HTTP_'):
             new_key = k[5:]
+
         if new_key is not None:
             headers[new_key.replace('_', '-').title()] = v
     response['headers'] = headers
-
-    response['origin'] = request.META.get('REMOTE_ADDR')
-    response['url'] = request.get_raw_uri()
 
     return HttpResponse(json.dumps(response, sort_keys=True))
