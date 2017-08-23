a6d44311d7dd8edb7495c15c3ae66a45c94fa95c
Jeremy Boyd <boydjj@gmail.com>
2017-08-22 23:22:49 -0500

    Add RequestLogEntry model for httpbucket

---
+++ httpbucket/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class RequestLogEntry(models.Model):
+    # we get `id` field as Primary Key by default
+
+    # OPTIONS is the longest method name
+    method = models.CharField(max_length=7)
+
+    # probably max of 2000 chars, but why limit it?
+    uri = models.TextField()
+
+    # some fields are specialized!
+    origin = models.GenericIPAddressField()
+
+    # just a JSON blob
+    headers = models.TextField()
+
+    # another JSON blob
+    args = models.TextField()
